9d6af17d7c42519caf699e98b05f079a9784f0a8
Moreaux19 <basargin19@gmail.com>
2024-12-12 19:46:04 +0300

    Import images

--- src/components/ArrowComponent.jsx
+++ src/components/ArrowComponent.jsx
@@ -1,3 +1,12 @@
+// import positive from '../assets/positive.svg';
+// import negative from '../assets/negative.svg';
+const green = { backgroundImage: "url('./src/assets/green.svg')" };
+const red = { backgroundImage: "url('./src/assets/red.svg')" };
+const grey = { backgroundImage: "url('./src/assets/grey.svg')" };
+const negative = <img src="./src/assets/negative.svg" alt="minus" />;
+const postive = <img src="./src/assets/positive.svg" alt="plus" />;
+console.log(grey);
+
 export default function ArrowComponent({
   firstBlockHeight,
   secondBlockHeight,
@@ -11,24 +20,18 @@ export default function ArrowComponent({
   const settingRectangle = value => {
     let pointer = null;
     if (value > 0) {
-      pointer = <img src="./src/assets/positive.svg" alt="Plus" />;
+      pointer = postive;
       return (
-        <div
-          className="arrow__difference"
-          style={{ backgroundImage: "url('./src/assets/green.svg')" }}
-        >
+        <div className="arrow__difference" style={green}>
           <p className="arrow__pointer">
             {pointer} +{value}
           </p>
         </div>
       );
     } else if (value < 0) {
-      pointer = <img src="./src/assets/negative.svg" alt="Minus" />;
+      pointer = negative;
       return (
-        <div
-          className="arrow__difference"
-          style={{ backgroundImage: "url('./src/assets/red.svg')" }}
-        >
+        <div className="arrow__difference" style={red}>
           <p className="arrow__pointer">
             {pointer} {value}
           </p>
@@ -36,10 +39,7 @@ export default function ArrowComponent({
       );
     } else {
       return (
-        <div
-          className="arrow__difference"
-          style={{ backgroundImage: "url('./src/assets/grey.svg')" }}
-        >
+        <div className="arrow__difference" style={grey}>
           <p className="arrow__pointer">{value}</p>
         </div>
       );
